75dbc201b6e757105c6f82b0ff505f3e0462a0a1
Nick Sterling <nicholas.v.sterling@gmail.com>
2020-08-19 14:12:16 -0600

    Regenerated the Kubernetes workshop

--- workshops/k8s/en.search.min.eb9a988b95fb597bebcf56c392dbcd9e8add21d684697fe29dcfe07faf70e071.js
+++ workshops/k8s/en.search.min.eb9a988b95fb597bebcf56c392dbcd9e8add21d684697fe29dcfe07faf70e071.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.37ea7f8bbd8f3042b647481ff198c0b2f8175df1b685d38ce7f64e9ea0b6de77.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.c783f7b22f52d728b9898e4fc688783f9a2f2192ff6a0f36a366ee013bf8ac19.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
